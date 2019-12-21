668d9e733b72678b68d63a8195e0b0a85ab0e9a7
rendaras <universof2@gmail.com>
2019-12-21 15:02:10 +0200

    carusel 1 beta

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,29 @@
 $(function() {
+  $('.formatsTextP').fadeOut(0);
+  $('#item1').fadeIn(1000);
 
-	// Custom JS
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
 
 });
+
+$('.item').on('click', function(e) {
+  console.log(e.target.className);
+  $('.formatsTextP').fadeOut(0);
+  $('#'+e.target.className).fadeIn(1000);
+});
+    
